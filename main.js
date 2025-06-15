2e5d8fb59a6bb7405f8b36ebdebc80d6deac143f
w1ndfallen <dlyashkolu192@gmail.com>
2025-06-15 21:48:49 +0300

    main.js

--- main.js
+++ main.js
@@ -1,48 +1,58 @@
-console.log("Task 10:");
+//Task 1.2
+console.log("Task 1.2:");
 let a = 4;
 let x = a + 2.5;
-console.log("a =", a);
-console.log("x = a + 2.5 =", x);
+let result = Math.sqrt(a) <= x && x < 2.5;
+console.log(result);
+//Task 2.1
+console.log("Task 2.1:");
+let alpha = 15.3;
+let beta = -0.012;
+let x = Math.sin(alpha + 2.3) - Math.tan(beta - 3.2);
+let y = Math.sin(alpha - beta) * Math.sin(alpha - beta);
+let n = Math.atan(Math.sin(x) * Math.sin(x) + Math.tan(y) * Math.tan(y));
+console.log("Result is: " + n);
+//Task 3.1
+console.log("Task 10:");
+let x = 2;
+let y = null;
+if(Math.abs(x) > 1){
+    y = Math.cbrt(Math.tan(x) + Math.log(x*x));
+} else if(x <= 1){
+    y = Math.exp(x) + 1;
+} else if(x === 1){
+    y = 1;
+}
 
-let sqrtA = Math.sqrt(a);
-console.log("√a =", sqrtA);
-
-let condition1 = sqrtA <= x;
-let condition2 = x < 2.5;
-let finalCondition = condition1 && condition2;
-
-console.log("√a ≤ x:", condition1, "(", sqrtA, "≤", x, ")");
-console.log("x < 2.5:", condition2, "(", x, "< 2.5)");
-console.log("√a ≤ x < 2.5:", finalCondition);
-
-console.log("\nTesting with different values a:");
-
-console.log("Task 10.1:");
-a = 1;
-x = a + 2.5;
-sqrtA = Math.sqrt(a);
-condition1 = sqrtA <= x;
-condition2 = x < 2.5;
-finalCondition = condition1 && condition2;
-console.log("a =", a, ", x =", x, ", √a =", sqrtA);
-console.log("√a ≤ x < 2.5:", finalCondition);
+if (y === null){
+    console.log("Wrong input");
+} else{
+    console.log(y);
+}
+//Task 4.1
+console.log("Task 10:");
+for(let x = 3.2; x <= 5.2; x += 0.4){
+    let y = (3*x - 2)/(2*Math.atan(Math.abs(x)) + 1);
+    console.log(y);
+}
 
 console.log("Task 10.2:");
-a = 0.25;
-x = a + 2.5;
-sqrtA = Math.sqrt(a);
-condition1 = sqrtA <= x;
-condition2 = x < 2.5;
-finalCondition = condition1 && condition2;
-console.log("a =", a, ", x =", x, ", √a =", sqrtA);
-console.log("√a ≤ x < 2.5:", finalCondition);
+for(let x = 2; x <= 5; x += 1){
+    let y = (3*x - 2)/(2*Math.atan(Math.abs(x)) + 1);
+    console.log(y);
+}
+//Task 5.1
+console.log("Task 10:");
+let S = 0;
+for(let n = 2; n <= 5; n++){
+    S += Math.pow(-1, n) * (4*n - 5)/(2*n + 2);
+}
+console.log(S);
 
-console.log("Task 10.3:");
-a = 6.25;
-x = a + 2.5;
-sqrtA = Math.sqrt(a);
-condition1 = sqrtA <= x;
-condition2 = x < 2.5;
-finalCondition = condition1 && condition2;
-console.log("a =", a, ", x =", x, ", √a =", sqrtA);
-console.log("√a ≤ x < 2.5:", finalCondition);
+console.log("Task 10.2:");
+let m = 2;
+let P = 1;
+for(let k = 1; k <= m; k++){
+    P *= (1-1/k)/(Math.pow(k,2)*(k+3)*(k+4));
+}
+console.log(P);
